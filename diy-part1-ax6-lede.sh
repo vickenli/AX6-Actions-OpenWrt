e9d91b2dff8425d6c34d654a33567f1411c5bc54
vickenli <89629647+vickenli@users.noreply.github.com>
2023-06-15 18:02:11 -0700

    Update diy-part1-ax6-lede.sh

--- diy-part1-ax6-lede.sh
+++ diy-part1-ax6-lede.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # https://stackoverflow.com/questions/3731513/how-do-you-type-a-tab-in-a-bash-here-document
 TAB=$'\t'
